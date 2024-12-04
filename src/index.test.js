8c99fdc0a3c86249b40152a9be26757b200551a1
jonorl <39122230+jonorl@users.noreply.github.com>
2024-12-04 16:55:23 +0300

    Completed up to exercise 3

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,24 @@
-const sum = require('./js/index');
+const { capitalise, reverseString, calculator } = require("./js/index");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test("Capitalise test", () => {
+  expect(capitalise("jonathan")).toBe("Jonathan");
 });
+
+test("Reverse test", () => {
+  expect(reverseString("Jonathan")).toBe("nahtanoJ");
+});
+
+test("Calculator add test", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("Calculator subtract test", () => {
+  expect(calculator.subtract(1, 2)).toBe(-1);
+});
+test("Calculator divide test", () => {
+  expect(calculator.divide(12, 4)).toBe(3);
+});
+test("Calculator multiply test", () => {
+    expect(calculator.multiply(12, 4)).toBe(48);
+  });
+  
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,33 @@
-function sum(a, b) {
-  return a + b;
+function capitalise(str) {
+  let arr = Array.from(str);
+  arr[0] = arr[0].toString().toUpperCase();
+  return arr.join("");
 }
-module.exports = sum;
+
+function reverseString(str) {
+  let arr = Array.from(str);
+  let tmp = [];
+  for (let i = arr.length; i !== -1; i--) {
+    tmp.push(arr[i]);
+  }
+  return tmp.join("");
+}
+
+const calculator = {
+  add: function add(num1, num2) {
+    return num1 + num2;
+  },
+  subtract: function subtract(num1, num2) {
+    return num1 - num2;
+  },
+  divide: function divide(num1, num2) {
+    return num1 / num2;
+  },
+  multiply: function multiply(num1, num2) {
+    return num1 * num2;
+  },
+};
+
+module.exports = { capitalise, reverseString, calculator };
+
+console.log(capitalise("jonathan"));
